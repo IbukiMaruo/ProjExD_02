b2a19535f6956ddb3aa3043e66fbaf88667ffcd4
Ibuki <c0b2213810@edu.teu.ac.jp>
2023-06-20 14:21:06 +0900

    練習問題2

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -19,6 +19,8 @@ def main():
     y = random.randint(0,HEIGHT)
     bb_rct = bb_img.get_rect()
     bb_rct.center = x,y 
+    vx,vy = +5, -5
+
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -28,10 +30,11 @@ def main():
 
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, [900, 400])
+        bb_rct.move_ip(vx,vy)
         screen.blit(bb_img,bb_rct)
         pg.display.update()
         tmr += 1
-        clock.tick(10)
+        clock.tick(50)
 
 
 if __name__ == "__main__":
